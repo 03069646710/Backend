664b889da1a2ad8a2ef3639cf624e2a599d6eb4d
APP Developer <umair.577155@gmail.com>
2022-12-04 07:48:44 +0500

    intial commit

---
+++ BZUAttendanceBackend-main/README.md
@@ -0,0 +1,21 @@
+# BZUAttendance Backend Server
+
+**This is fake backend server for BZU Attendance App with static data,
+and It does not save data into database.**
+
+**Technologies Used:**
+- NodeJS https://nodejs.org/en/download/
+- Express https://expressjs.com/
+
+**Steps to Run:**
+
+1. Install NodeJS
+
+2. Install project packages. It will install project dependencies
+`npm install`
+
+3. Install nodemon
+`npm install nodemon --global`
+
+3. Run backend API as
+`nodemon app.js`
---
+++ BZUAttendanceBackend-main/app.js
@@ -0,0 +1,116 @@
+const express = require('express')
+const app = express()
+const port = 80
+
+app.use(express.json());
+
+app.post('/students/register', (req, res) => {
+  console.log('request recived');
+  console.log(req.body);
+  res.json(req.body);
+  console.log('response sent');
+
+})
+
+app.post('/students/login', (req, res) => {
+  console.log('request recived');
+  console.log(req.body);
+  
+  var rollnumber = req.body['RollNumber'];
+  var password = req.body['Password'];
+  var result = {};
+  if(rollnumber == 'LDBTT-1923-01' && password == 'pass123') {
+    result = {
+      Success: true,
+      Name: "Rana Akram",
+    };
+  } else {
+    result = {
+      Success: false,
+      ErrorMessage: "Wrong RollNumber or Password"
+    };
+  }
+  
+  res.json(result);
+  console.log('response sent');
+
+})
+
+app.post('/timetable', (req, res) => {
+  console.log('request recived');
+
+  var result = 
+  [
+    {
+      "Date": "20221125",
+      "TimeSlot": 1,
+      "SubjectCode": "IT205",
+      "Subject": "Data Structure",
+      "Teacher": "Kamran Qadir"
+    },
+    {
+      "Date": "20221125",
+      "TimeSlot": 2,
+      "SubjectCode": "IT307",
+      "Subject": "Visual Programming",
+      "Teacher": "Kamran Qadir"
+    },
+    {
+      "Date": "20221125",
+      "TimeSlot": 3,      
+      "SubjectCode": "IT403",
+      "Subject": "Mobile App Development",
+      "Teacher": "Kamran Qadir"
+    }
+  ];
+
+  res.json(result);
+  console.log('response sent');
+})
+
+app.post('/attendance/history', (req, res) => {
+  console.log('request recived');
+
+  var result = 
+  [
+    {
+      "Date": "20221125",
+      "TimeSlot": 1,
+      "SubjectCode": "IT205",
+      "Subject": "Data Structure",
+      "Teacher": "Kamran Qadir",
+      "Status": "PRESENT",
+    },
+    {
+      "Date": "20221125",
+      "TimeSlot": 2,
+      "SubjectCode": "IT307",
+      "Subject": "Visual Programming",
+      "Teacher": "Kamran Qadir",
+      "Status": "PRESENT",
+    },
+    {
+      "Date": "20221125",
+      "TimeSlot": 3,
+      "SubjectCode": "IT403",
+      "Subject": "Mobile App Development",
+      "Teacher": "Kamran Qadir",
+      "Status": "ABSENT",
+    },
+  ];
+
+  res.json(result);
+  console.log('response sent');
+})
+
+app.post('/attendance/mark', (req, res) => {
+  console.log('request recived');
+  console.log(req.body);
+  res.json(req.body);
+  console.log('response sent');
+
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
